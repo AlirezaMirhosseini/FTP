d6ecfba09b6f220d6e3ee7370af9628dac31a2cb
MohammadYasinKarbasian <m.y.karbasian1@gmail.com>
2022-04-19 19:30:09 +0430

    CD and LIST added
    CD and LIST fundumentals programed

--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,7 @@
+from posixpath import dirname
 from socket import *
 import os
-
+import pickle
 
 BASE_DIR = os.getcwd()
 CURRENT_PATH = os.getcwd()
@@ -9,20 +10,36 @@ def HELP():
     return 'These are available commands\n1.HELP\n2.LIST\n3.DWLD\n4.PWD\n5.CD\n'
 
 def LIST():
-    pass
+    return ''.join(f'{obj}  {str(os.path.getsize(obj))} bits\n' for obj in os.listdir(CURRENT_PATH))
 
 def DWLD(filePath):
     pass
 
 def PWD():
+    global BASE_DIR
     real_path = os.getcwd()
-    return '/'+real_path[len(BASE_DIR):]
+    if(BASE_DIR != real_path):
+        return real_path[len(BASE_DIR):]
+    else:
+        return '/'
 
 def CD(dirName):
-    
-    CURRENT_PATH = CURRENT_PATH + dirName
-    os.chdir(CURRENT_PATH)
-    return CURRENT_PATH[len(BASE_DIR):]
+    dirName = ' '.join(st for st in dirName)
+    if dirName[len(dirName)-1] != '/':
+        dirName = '/' + dirName
+    global CURRENT_PATH,BASE_DIR
+    if dirName == '..':
+        counter = len(CURRENT_PATH)-1
+        while(len(CURRENT_PATH)!=len(BASE_DIR) and CURRENT_PATH[counter]!='/'):
+            counter = counter -1
+        if CURRENT_PATH[counter] == '/': counter = counter -1
+        CURRENT_PATH =  CURRENT_PATH[:counter]    
+        return PWD()
+            
+    else:
+        CURRENT_PATH = CURRENT_PATH + dirName
+        os.chdir(CURRENT_PATH)
+        return PWD()
 
 
 
@@ -39,17 +56,21 @@ while(True):
     command = command.split()
     if command[0] == 'HELP':
         data = HELP()
+
     elif command[0] == 'LIST':
         data = LIST()    
+
     elif command[0] == 'PWD':
         data = PWD()
-    elif command == 'DWLD':
+
+    elif command[0] == 'DWLD':
         data = DWLD(command[1:])
-    elif command == 'CD':
-        data = CD(command[1:])    
+
+    elif command[0] == 'CD':
+        data = CD(command[1:])   
+ 
     else:
         data = 'command not found'
-
     privateSocket.send(data.encode())
     #send to client
 privateSocket.close()
